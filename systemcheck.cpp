02eb88de71212a9b4300f00df75053961b10c7bd
Harshkr8878 <harshkr8878@gmail.com>
2025-01-24 00:48:44 +0530

    new file add

--- systemcheck.cpp
+++ systemcheck.cpp
@@ -1,1 +1,17 @@
-#just for checking purpose
+//just for checking purpose
+#include<iostream>
+using namespace std;
+void printArray(){
+
+}
+int main(){
+    int num[15];
+    cout<<"value of 14 index "<<num[14]<<endl;
+
+    int second[3]={5,7,11};
+    //accesing the element 
+    cout<<"value of 2 index "<<second[2]<<endl;
+
+    cout<<"fine"<<endl;
+
+}
